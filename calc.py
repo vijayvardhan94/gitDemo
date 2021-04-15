7b855f23ae7e16f1de75a75698be08b71361ea6a
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-04-14 23:21:08 -0700

    list and list sum

--- calc.py
+++ calc.py
@@ -4,5 +4,12 @@ def calc(a,b):
     product = a * b
     division = a / b
     print(sum, difference, product, division)
+    list1 = [sum, difference, product, division]
+    listsum = 0
+    for i in range(len(list1)):
+        listsum += list1[i]
+    
+    print(list1)
+    print(listsum)
 
 calc(3,4)
